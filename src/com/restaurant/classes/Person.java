3503d398cab7cc3336b63ed7559e0ed087d69628
Luis martinez <Luis.jorge.martinez28@gmail.com>
2019-08-02 23:18:26 -0400

    first commit

---
+++ src/com/restaurant/classes/Person.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.restaurant.classes;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Luis
+ */
+public class Person {
+      //Aqui creamos la variables que vamos a necesitar para manejar los datos de mysql
+    private int IdPerson;
+    private String Name,
+            LastName1,
+            LastName2,
+            Gender,
+            Adress,
+            Telephone,
+            Phone,
+            Cedula;
+    private Date DateReg;
+    
+}
---
+++ src/com/restaurant/classes/Product.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.restaurant.classes;
+
+/**
+ *
+ * @author Luis 
+ */
+public class Product {
+    private int IdProduct, KindOfProduct ;
+    private String Description;
+    private double Price, Stock;
+    private boolean ITBIS;
+    
+    
+    
+}
